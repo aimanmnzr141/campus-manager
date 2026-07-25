4e8a9469c4dbfbb2f0b59f5c08dc4e1b9ca12d86
aimanmnzr141 <aimanmanzoor141@gmail.com>
2026-07-26 00:13:05 +0530

    updated views and urls

--- students/views.py
+++ students/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Welcome to The Student Management System ")
+
+
+def add_student(request):
+    return HttpResponse("Add Student Page")
+
+
+def delete_student(request):
+    return HttpResponse("Delete Student Page")
