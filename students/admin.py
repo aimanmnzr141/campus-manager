daaa97acd2c0b1e834ad2acdf0d3b8170cee17e2
aimanmnzr141 <aimanmanzoor141@gmail.com>
2026-07-25 19:38:27 +0530

    registered model with admin

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+# "."->(current package) in models here indicate that both admin and models are in the same students app
+from .models import Student
+
 # Register your models here.
+admin.site.register(Student)
