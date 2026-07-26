611b4a13a2713531cbe25e9e79d6360dd8ec137f
aimanmnzr141 <aimanmanzoor141@gmail.com>
2026-07-26 18:58:51 +0530

    Initial implementation of django student management system

--- students/views.py
+++ students/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student
+from .forms import StudentForm
 
 
 # Create your views here.
@@ -7,9 +8,43 @@ def home(request):
     return render(request, "home.html")
 
 
+def student_list(request):
+    # Go to student table and give me all students data
+    students = Student.objects.all()
+
+    return render(request, "student_list.html", {"students": students})
+
+
 def add_student(request):
-    return HttpResponse("Add Student Page")
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+        return redirect("student_list")
+
+    else:
+        form = StudentForm()
+        return render(request, "add_student.html", {"form": form})
 
 
-def delete_student(request):
-    return HttpResponse("Delete Student Page")
+def edit_student(request, id):
+    student = get_object_or_404(Student, id=id)
+
+    if request.method == "POST":
+        form = StudentForm(request.POST, instance=student)
+
+        if form.is_valid():
+            form.save()
+            return redirect("student_list")
+
+    else:
+        form = StudentForm(instance=student)
+
+    return render(request, "edit_student.html", {"form": form})
+
+
+def delete_student(request, id):
+    student = get_object_or_404(Student, id=id)
+    student.delete()
+    return redirect(student_list)
